8453c6f30ca547b23cafa0f8034df2026fc23986
Awynimantha <awynimantha@gmail.com>
2024-11-29 22:37:35 +0530

    dense_layer init

---
+++ dense_layer.py
@@ -0,0 +1,8 @@
+import numpy as np
+class DenseLayer:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = np.random.rand(n_inputs, n_neurons)
+        self.biases = np.zeros(1, n_neurons)
+    
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
